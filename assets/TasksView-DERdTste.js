16b6a800eec978f480aa446b9f8054f102861196
d.hripunov <ipunov@def.team>
2024-09-13 20:53:42 +0300

    Updates

---
+++ assets/TasksView-DERdTste.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as s}from"./index-Nny0az1Z.js";const n={};function o(r,t){return c(),s("h1",null,"Tasks")}const _=e(n,[["render",o]]);export{_ as default};
--- assets/TasksView-TI5MOk1z.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as s}from"./index-xo9ISFZr.js";const n={};function o(r,t){return c(),s("h1",null,"Tasks")}const _=e(n,[["render",o]]);export{_ as default};
